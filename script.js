4e7663440c42a5da6ef5c1aca7eca3fbf9e440dd
Helen <hellenpolo10@gmail.com>
2025-01-22 22:35:20 -0500

    Hola, esto es una prueba.

---
+++ script.js
@@ -0,0 +1,10 @@
+const n = document.getElementById("input");
+function button(input) {
+    n.value += input
+}
+function inputClear() {
+    n.value = "";
+}
+function calculate() {
+    n.value = eval(n.value)
+}
